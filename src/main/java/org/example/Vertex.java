90826d9b2414cfd6dead6fe6f026f489c30322c8
nurmullaevaa-jpg <nurmullaevaa@gmail.com>
2026-05-12 21:37:59 +0500

    Implemented Vertex.java

---
+++ src/main/java/org/example/Vertex.java
@@ -0,0 +1,18 @@
+package org.example;
+import java.util.*;
+
+class Vertex<V> {
+    private V data;
+    private Map<Vertex<V>, Double> adjacentVertices = new HashMap<>();
+
+    public Vertex(V data) {
+        this.data = data;
+    }
+
+    public void addAdjacentVertex(Vertex<V> destination, double weight) {
+        adjacentVertices.put(destination, weight);
+    }
+
+    public V getData() { return data; }
+    public Map<Vertex<V>, Double> getAdjacentVertices() { return adjacentVertices; }
+}
