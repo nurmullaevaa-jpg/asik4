f99826824a63f8e428025ee7282adf7615701345
nurmullaevaa-jpg <nurmullaevaa@gmail.com>
2026-05-12 21:44:25 +0500

    Changed logic for search

--- src/main/java/org/example/Search.java
+++ src/main/java/org/example/Search.java
@@ -2,22 +2,29 @@ package org.example;
 import java.util.*;
 
 abstract class Search<V> {
-    protected Vertex<V> source;
     protected Map<Vertex<V>, Vertex<V>> edgeTo = new HashMap<>();
+    protected Vertex<V> startVertex;
 
-    public Search(Vertex<V> source) {
-        this.source = source;
+    public Search(Vertex<V> startVertex) {
+        this.startVertex = startVertex;
     }
 
-    public boolean hasPathTo(Vertex<V> v) {
-        return edgeTo.containsKey(v);
-    }
+    // Метод pathTo возвращает Iterable<V>, как ожидает Main
+    public Iterable<V> pathTo(V targetData) {
+        LinkedList<V> path = new LinkedList<>();
+        Vertex<V> target = null;
 
-    public Iterable<Vertex<V>> pathTo(Vertex<V> v) {
-        if (!hasPathTo(v)) return null;
-        LinkedList<Vertex<V>> path = new LinkedList<>();
-        for (Vertex<V> x = v; x != null; x = edgeTo.get(x)) {
-            path.addFirst(x);
+        for (Vertex<V> v : edgeTo.keySet()) {
+            if (v.getData().equals(targetData)) {
+                target = v;
+                break;
+            }
+        }
+
+        if (target == null && !startVertex.getData().equals(targetData)) return null;
+
+        for (Vertex<V> x = target; x != null; x = edgeTo.get(x)) {
+            path.addFirst(x.getData());
         }
         return path;
     }
