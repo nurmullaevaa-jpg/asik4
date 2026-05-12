94d1fa65407b36b605ddd9e14bb8f809cc9b0145
nurmullaevaa-jpg <nurmullaevaa@gmail.com>
2026-05-12 21:43:38 +0500

    Changed logic for weighted graph class

--- src/main/java/org/example/WeightedGraph.java
+++ src/main/java/org/example/WeightedGraph.java
@@ -1,11 +1,23 @@
 package org.example;
 import java.util.*;
 class WeightedGraph<V> {
-    private List<Vertex<V>> vertices = new ArrayList<>();
+    private final Map<V, Vertex<V>> vertices = new HashMap<>();
+    private final boolean undirected;
 
-    public void addVertex(Vertex<V> vertex) {
-        vertices.add(vertex);
+    public WeightedGraph(boolean undirected) {
+        this.undirected = undirected;
     }
 
-    public List<Vertex<V>> getVertices() { return vertices; }
+    public void addEdge(V source, V dest, double weight) {
+        if (!vertices.containsKey(source)) vertices.put(source, new Vertex<>(source));
+        if (!vertices.containsKey(dest)) vertices.put(dest, new Vertex<>(dest));
+
+        vertices.get(source).addAdjacentVertex(vertices.get(dest), weight);
+        if (undirected) {
+            vertices.get(dest).addAdjacentVertex(vertices.get(source), weight);
+        }
+    }
+
+    public Vertex<V> getVertex(V data) { return vertices.get(data); }
+    public Collection<Vertex<V>> getAllVertices() { return vertices.values(); }
 }
